de41bb72de19db6733287179e9ba4b9fcdcc9f3a
DioneMartins <vitoria.martins@mprj.mp.br>
2022-07-25 20:05:19 -0300

    Adicionado tutorial

--- codigo/perfil.c
+++ codigo/perfil.c
@@ -5,7 +5,7 @@
 #include "cartao.c"
 #include "game.c"
 
-void v_menu();
+void v_menu(); void tutorial();
 
 int main() {
     char c='0';
@@ -15,8 +15,9 @@ int main() {
         flush();
         clear();
         if(c=='1') i_jogo();
-        else if(c=='2') m_cartoes();
-        else if(c=='3') break;
+        else if(c=='2') tutorial();
+        else if(c=='3') m_cartoes();
+        else if(c=='4') break;
     }
     printf("Espero que tenha se divertido!\n");
     return 0;
@@ -28,4 +29,24 @@ void v_menu(){
         "2 Menu de cartoes\n"
         "3 Sair\n"
     );
+}
+
+void tutorial(){
+    printf(
+        "Tela de jogo:\n"
+        "Mediador: <--- O Jogador que nao da palpites, mas ganha pontos por cada dica nao lida.\n"
+        "Jogador da vez: <--- O Jogador jogando agora. Ganha pontos se acertar ou por instrucoes.\n"
+        "jogador(0) <--- O nome de um jogador e sua posicao no tabuleiro/placar.\n"
+        "Dicas:  1 2 3 4 5 6 7 8 9 X X X X X X X X X X X <--- Dicas do cartao. Dicas ja usadas sao\n"
+        "marcadas por um x.\n"
+        "\n\nPalpite a qualquer hora: Se o jogador tiver uma ficha (se tiver, naquela rodada,\n"
+        "pego uma dica de 'Palpite a qualquer hora'), ele pode digitar:\n"
+        "!pqh nome\n"
+        "Onde nome e o nome do jogador. Voce pode digitar o comando no seguinte momento:\n"
+        "Jogador ... Qual e o seu palpite? [50]\n"
+        "Isso nao remove a vez do jogador atual e ele pode fazer isso logo depois.\n"
+        "E isso.\n"
+    );
+    flush();
+    clear();
 }
