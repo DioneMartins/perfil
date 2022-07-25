77fd3797eb2c59305d8339a61da572d3a8b88d36
DioneMartins <vitoria.martins@mprj.mp.br>
2022-07-25 19:12:52 -0300

    Finalizado?

--- codigo/game.c
+++ codigo/game.c
@@ -3,7 +3,7 @@
 
 typedef struct _JOGADOR {
     char nome[50];
-    int ordem, casa, ficha, mediador;
+    int ordem, casa, ficha;
 } JOGADOR;
 
 typedef struct _CARTAO {
@@ -25,6 +25,7 @@ void palpite_a_qualquer_hora(JOGADOR jog[], int n, char *input, int jog_atual, R
 void palpite(JOGADOR jogador, RODADA rodada, char *input, int pontua, int *vence);
 int final_rodada(RODADA rodada, JOGADOR jogadores[], int n, int *med);
 void tela_final(JOGADOR jogadores[], int n);
+int is_okay(int r, int cartoes[5]);
 
 int i_jogo(){
     JOGADOR *jog;
@@ -60,6 +61,7 @@ int m_jogo(JOGADOR jogadores[], int count){
     int x=0; /*Int generico*/
     int andou=0; /*Boll para se o jogador ja andou*/
     char string_input[50]; int int_input=0; /*Inputs genericos*/
+    int ultimos_cartoes[5];
     RODADA rodada;
     FILE *file;
 
@@ -70,6 +72,16 @@ int m_jogo(JOGADOR jogadores[], int count){
         return 1;
     }
     total_c=conta_c();
+    if(total_c<6) {
+        printf("Nao e possivel iniciar um jogo com esse numero de cartoes.\n");
+        printf("Por favor, considere recuperar o arquivo inicial ou cadastrar novos cartoes.\n");
+        printf("Pressione enter para voltar ao menu inicial.\n");
+        flush();
+        return 1;
+    }
+    for(x=0; x<4; x++){
+        ultimos_cartoes[x]=0;
+    }
 
     printf("Perssione enter para comecar...\n");
     flush();
@@ -77,112 +89,126 @@ int m_jogo(JOGADOR jogadores[], int count){
     /*Loop do jogo*/
     while(fim_do_jogo==0){
         /*Nova rodada*/
-        cartao_da_vez=randint(total_c);
-        rodada.cartao=escolhe_cartao(file, cartao_da_vez);
+        while(1){
+            cartao_da_vez=randint(total_c);
+            if(is_okay(cartao_da_vez, ultimos_cartoes)){
+                for(x=0; x<4; x++){
+                    ultimos_cartoes[x]=ultimos_cartoes[x+1];
+                }
+                ultimos_cartoes[4]=cartao_da_vez;
+                rodada.cartao=escolhe_cartao(file, cartao_da_vez);
+            }
+        }
 
         for(x=0; x<20; x++) rodada.cartao.dicas_usadas[x]=0; /*Reseta o array de dicas usadas*/
         rodada.nd=0; /*Reseta o numero de dicas usadas*/
 
-        vez=med+1; if(vez>=count) vez=1;
+        vez=med+1; if(vez>=count) vez=0;
+
+        rodada.vencedor=-1;
+
+        for(x=0; x<count; x++) jogadores[x].ficha=0;
 
         while(1){
-            rodada.vencedor=-1;
-            printf("Mediador: %s(%d)\n", jogadores[med].nome, jogadores[med].casa);
-            printf("Jogador da vez: %s(%d)\n", jogadores[vez].nome, jogadores[vez].casa);
-            printf("\nEu sou... "); printc(rodada.cartao.categoria);
-            exibe_dicas(rodada.cartao.dicas_usadas, (sizeof(rodada.cartao.dicas_usadas)/sizeof(int)));
-            printf("\nPronto, jogador %s... Que dica voce quer? [1-20] ", jogadores[vez].nome);
-            scanf("%d", &int_input);
-            uso=dica_ja_usada(rodada.cartao.dicas_usadas, int_input);
-            if(int_input<1 || int_input>20 || uso){
-                if(!uso) printf("Opcao invalida\n");
-                else printf("Dica ja pedida.\n");
-            }
-            else{
-                printf("Atencao, mesa... ");
-                printd(0, rodada.cartao.dicas[int_input-1], 0);
-                rodada.cartao.dicas_usadas[rodada.nd]=int_input;
-                rodada.nd++;
-                if(rodada.cartao.dicas[int_input-1][0]=='!'){
-                    flush();
-                    comando=markd(rodada.cartao.dicas[int_input-1]);
-                    switch(comando){
-                        case 2:
-                            jogadores[vez].ficha=1;
-                            break;
-                        case 3:
-                            jogadores[vez].casa+=((rodada.cartao.dicas[int_input-1][2])-'0');
-                            break;
-                        case 4:
-                            jogadores[vez].casa-=((rodada.cartao.dicas[int_input-1][2])-'0');
-                            if(jogadores[vez].casa<1) jogadores[vez].casa=1;
-                            break;
-                        case 5:
-                            while(1){
-                                andou=0;
-                                printf("Que jogador você escolhe? (exceto si mesmo) \n");
-                                fgets(string_input, 50, stdin); flush();
-                                string_input[strlen(string_input)-1] = '\0';
-                                if(strcmp(string_input, jogadores[vez].nome)==0) printf("Voce nao pode escolher a si mesmo!");
-                                else {
-                                        for(x=0; x<count; x++){
-                                        if(strcmp(string_input, jogadores[x].nome)==0) {
-                                            jogadores[x].casa+=((rodada.cartao.dicas[int_input-1][3])-'0');
-                                            andou=1;
-                                            break;
+            uso=0;
+            if(vez!=med){
+                comando=0;
+                printf("Mediador: %s(%d)\n", jogadores[med].nome, jogadores[med].casa);
+                printf("Jogador da vez: %s(%d)\n", jogadores[vez].nome, jogadores[vez].casa);
+                printf("\nEu sou... "); printc(rodada.cartao.categoria);
+                exibe_dicas(rodada.cartao.dicas_usadas, (sizeof(rodada.cartao.dicas_usadas)/sizeof(int)));
+                printf("\nPronto, jogador %s... Que dica voce quer? [1-20] ", jogadores[vez].nome);
+                scanf("%d", &int_input);
+                uso=dica_ja_usada(rodada.cartao.dicas_usadas, int_input);
+                if(int_input<1 || int_input>20 || uso){
+                    if(!uso) printf("Opcao invalida\n");
+                    else printf("Dica ja pedida.\n");
+                }
+                else{
+                    printf("Atencao, mesa... ");
+                    printd(0, rodada.cartao.dicas[int_input-1], 0);
+                    rodada.cartao.dicas_usadas[rodada.nd]=int_input;
+                    rodada.nd++;
+                    if(rodada.cartao.dicas[int_input-1][0]=='!'){
+                        comando=markd(rodada.cartao.dicas[int_input-1]);
+                        switch(comando){
+                            case 2:
+                                jogadores[vez].ficha=1;
+                                break;
+                            case 3:
+                                jogadores[vez].casa+=((rodada.cartao.dicas[int_input-1][2])-'0');
+                                break;
+                            case 4:
+                                jogadores[vez].casa-=((rodada.cartao.dicas[int_input-1][2])-'0');
+                                if(jogadores[vez].casa<1) jogadores[vez].casa=1;
+                                break;
+                            case 5:
+                                while(1){
+                                    andou=0;
+                                    printf("Que jogador você escolhe? (exceto si mesmo) \n");
+                                    flush(); fgets(string_input, 50, stdin);
+                                    string_input[strlen(string_input)-1] = '\0';
+                                    if(strcmp(string_input, jogadores[vez].nome)==0) printf("Voce nao pode escolher a si mesmo!");
+                                    else {
+                                            for(x=0; x<count; x++){
+                                            if(strcmp(string_input, jogadores[x].nome)==0) {
+                                                jogadores[x].casa+=((rodada.cartao.dicas[int_input-1][3])-'0');
+                                                andou=1;
+                                                break;
+                                            }
                                         }
+                                        if(!andou) printf("Jogador nao encontrado.\n");
+                                        else break;
                                     }
-                                    if(!andou) printf("Jogador nao encontrado.\n");
-                                    else break;
                                 }
-                            }
-                            break;
-                        case 6:
-                            while(1){
-                                andou=0;
-                                printf("Que jogador você escolhe? (exceto si mesmo) \n");
-                                fgets(string_input, 50, stdin);
-                                string_input[strlen(string_input)-1] = '\0';
-                                if(strcmp(string_input, jogadores[vez].nome)==0) printf("Voce nao pode escolher a si mesmo!\n");
-                                else {
-                                        for(x=0; x<count; x++){
-                                        if(strcmp(string_input, jogadores[x].nome)==0) {
-                                            jogadores[x].casa-=((rodada.cartao.dicas[int_input-1][3])-'0');
-                                            andou=1;
-                                            break;
+                                break;
+                            case 6:
+                                while(1){
+                                    andou=0;
+                                    printf("Que jogador você escolhe? (exceto si mesmo) \n");
+                                    flush(); fgets(string_input, 50, stdin);
+                                    string_input[strlen(string_input)-1] = '\0';
+                                    if(strcmp(string_input, jogadores[vez].nome)==0) printf("Voce nao pode escolher a si mesmo!\n");
+                                    else {
+                                            for(x=0; x<count; x++){
+                                            if(strcmp(string_input, jogadores[x].nome)==0) {
+                                                jogadores[x].casa-=((rodada.cartao.dicas[int_input-1][3])-'0');
+                                                andou=1;
+                                                break;
+                                            }
                                         }
+                                        if(!andou) printf("Jogador nao encontrado.\n");
+                                        else break;
                                     }
-                                    if(!andou) printf("Jogador nao encontrado.\n");
-                                    else break;
                                 }
-                            }
-                            break;
+                                break;
+                        }
+                    }
+                    if(comando!=1){
+                        printf("\nJogador %s... Qual e o seu palpite? [50] ", jogadores[vez].nome);
+                        flush();
+                        fgets(string_input, 50, stdin);
+                        string_input[strlen(string_input)-1] = '\0'; /*Remove \n*/
+                        if(string_input[0]=='!')
+                            palpite_a_qualquer_hora(jogadores, count, string_input, vez, rodada);
+                        else palpite(jogadores[vez], rodada, string_input, vez, &rodada.vencedor);
                     }
                 }
-                if(comando!=1){
-                    printf("\nJogador %s... Qual e o seu palpite? [50] ", jogadores[vez].nome);
-                    flush();
-                    fgets(string_input, 50, stdin);
-                    string_input[strlen(string_input)-1] = '\0'; /*Remove \n*/
-                    if(string_input[0]=='!')
-                        palpite_a_qualquer_hora(jogadores, count, string_input, vez, rodada);
-                    else palpite(jogadores[vez], rodada, string_input, vez, &rodada.vencedor);
+                if(rodada.vencedor!=-1) {
+                    printf("Fim da rodada...\n"); 
+                    printf("\nPressione enter para continuar.\n"); flush(); clear();
+                    break;
                 }
-                if(rodada.vencedor==-1) {vez++; if(vez==med) vez++; if(vez>=count) vez=0;}
+                else if(rodada.nd==20) {
+                    printf("Fim da rodada...\n"); 
+                    printf("\nPressione enter para continuar.\n"); flush(); clear();
+                    break;
+                }
+                else{
+                    printf("\nPressione enter para continuar.\n"); flush(); clear();
+                }
             }
-            if(rodada.vencedor!=-1) {
-                printf("Fim da rodada...\n"); 
-                printf("\nPressione enter para continuar.\n"); flush(); clear();
-                break;
-            }
-            else if(rodada.nd==20) {
-                printf("Fim da rodada...\n"); 
-                printf("\nPressione enter para continuar.\n"); flush(); clear();
-                break;
-            }
-            else{
-                printf("\nPressione enter para continuar.\n"); flush(); clear();
-            }
+            if(!uso) {vez++; if(vez>=count) vez=0;}
         }
         fim_do_jogo = final_rodada(rodada, jogadores, count, &med);
         if(fim_do_jogo) break;
@@ -201,8 +227,6 @@ JOGADOR cadastra_jogador(int i){
     jogador.ordem=i;
     jogador.casa=1;
     jogador.ficha=0;
-    if(i==1) jogador.mediador=1;
-    else jogador.mediador=0;
     return jogador;
 }
 
@@ -247,25 +271,29 @@ int dica_ja_usada(int dicas_usadas[], int dica){
 void palpite_a_qualquer_hora(JOGADOR jog[], int n, char *input, int jog_atual, RODADA rodada){
     int i=0, encontrado=0;
     char nome[50];
-    puts(input);
-    for(i=0; i<strlen(input)-6; i++){
-        nome[i]=input[i+5];
-    }
-    puts(nome);
-    nome[i+1]='\0';
-    nome[strlen(input)-1]='\0';
-    for(i=0; i<n; i++){
-        if(strcmp(nome, input)==0){
-            encontrado=1;
-            if(jog[i].ficha==1) palpite(jog[i], rodada, input, jog_atual, &rodada.vencedor);
-            else {
-                printf("Voce nao tem palpites.\n");
-                palpite(jog[jog_atual], rodada, input, jog_atual, &rodada.vencedor);
+    if(input[5]=='\0') printf("Formato: !pqh jogador\n");
+    else{
+        for(i=0; i<strlen(input)-5; i++){
+            nome[i]=input[i+5];
+        }
+        nome[i]='\0';
+        for(i=0; i<n; i++){
+            if(strcmp(nome, jog[i].nome)==0){
+                encontrado=1;
+                if(jog[i].ficha==1) {
+                    palpite(jog[i], rodada, input, jog_atual, &rodada.vencedor);
+                    if(rodada.vencedor==-1) palpite(jog[jog_atual], rodada, input, jog_atual, &rodada.vencedor);
+                }
+                else {
+                    printf("Voce nao tem palpites.\n");
+                    palpite(jog[jog_atual], rodada, input, jog_atual, &rodada.vencedor);
+                }
+                break;
             }
         }
     }
     if(!encontrado){
-        printf("Jogador nao encontrado.\n");
+        printf("Jogador nao encontrado ou formato incorreto.\n");
         palpite(jog[jog_atual], rodada, input, jog_atual, &rodada.vencedor);
     }
 }
@@ -275,7 +303,6 @@ void palpite(JOGADOR jogador, RODADA rodada, char *input, int pontua, int *vence
         printf("\nJogador %s... Qual e o seu palpite? [50] ", jogador.nome);
         fgets(input, 50, stdin);
         input[strlen(input)-1]='\0';
-        flush();
     }
     uppercase(input, strlen(input)); /*Passa tudo para maiuscula*/
     if(strcmp(input, rodada.cartao.nome)==0) {
@@ -300,9 +327,9 @@ int final_rodada(RODADA rodada, JOGADOR jogadores[], int n, int *med){
     printf("\nNova pontuacao: \n");
     for(i=0; i<n; i++){
         printf("%s(%d)\n", jogadores[i].nome, jogadores[i].casa);
-        if(jogadores[i].casa>=40) vencido=1;
+        if(jogadores[i].casa>=134) vencido=1;
     }
-    *med=*med+1; if(*med>=n) med=0;
+    *med=*med+1; if(*med==n) *med=0;
     if(vencido) printf("Temos um vencedor!\n");
     else printf("\nO proximo mediador e... %s\n", jogadores[*med].nome);
     printf("\nPressione enter para continuar...\n"); flush(); clear();
@@ -310,7 +337,6 @@ int final_rodada(RODADA rodada, JOGADOR jogadores[], int n, int *med){
     /*Resetando tudo*/
     rodada.nd=0;
     rodada.vencedor=-1;
-    for(i=0; i<20; i++) rodada.cartao.dicas_usadas[i]=0;
 
     return vencido;
 }
@@ -332,4 +358,13 @@ void tela_final(JOGADOR jogadores[], int n){
     for(i=0; i<n; i++){
         printf("%s(%d)\n", jogadores[i].nome, jogadores[i].casa);
     }
+    printf("\nPressione enter para voltar ao menu principal...\n"); flush(); clear();
+}
+
+int is_okay(int r, int cartoes[5]){
+    int i=0, is_ok=1;
+    for(i=0; i<5; i++){
+        if(r==cartoes[i]) is_ok=0;
+    }
+    return is_ok;
 }
